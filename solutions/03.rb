4409accabe19575b0e4343e8b1cdfb6340b76014
Iliyan Bobev <bobev18@gmail.com>
2014-01-18 16:27:35 +0200

    switches the dirty old solution of 03, with the clean one turned in

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,3 +1,37 @@
+HTML_PREFIX = <<-PREDATA
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rendered Canvas</title>
+  <style type="text/css">
+    .canvas {
+      font-size: 1px;
+      line-height: 1px;
+    }
+    .canvas * {
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      border-radius: 5px;
+    }
+    .canvas i {
+      background-color: #eee;
+    }
+    .canvas b {
+      background-color: #333;
+    }
+  </style>
+</head>
+<body>
+  <div class="canvas">
+PREDATA
+
+HTML_SUFFIX = <<-POSTDATA
+  </div>
+</body>
+</html>
+POSTDATA
+
 module Graphics
   class Canvas
     attr_reader :width, :height
@@ -25,8 +59,7 @@ module Graphics
     end
 
     def render_as(renderer)
-      rendition = renderer.new @width, @height, @points
-      rendition.process
+      renderer.render(@width, @height, @points)
     end
 
     private
@@ -37,74 +70,34 @@ module Graphics
   end
 
   class Renderers
-
-    def initialize(width, height, points)
-      @width = width
-      @height = height
-      @points = points
-    end
-
     class Ascii < Renderers
-      def process
-        super("@", "-", "\n")
+      def self.render(width, height, points)
+        super("@", "-", "\n", width, height, points)
       end
     end
 
     class Html < Renderers
-      HTML_HEADER = <<-PREDATA
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Rendered Canvas</title>
-        <style type="text/css">
-          .canvas {
-            font-size: 1px;
-            line-height: 1px;
-          }
-          .canvas * {
-            display: inline-block;
-            width: 10px;
-            height: 10px;
-            border-radius: 5px;
-          }
-          .canvas i {
-            background-color: #eee;
-          }
-          .canvas b {
-            background-color: #333;
-          }
-        </style>
-      </head>
-      <body>
-        <div class="canvas">
-      PREDATA
-
-      HTML_FOOTER = <<-POSTDATA
-        </div>
-      </body>
-      </html>
-      POSTDATA
-
-      def process
-        HTML_HEADER + super("<b></b>", "<i></i>", "<br>") +\
-          HTML_FOOTER
+      def self.render(width, height, points)
+        HTML_PREFIX + super("<b></b>", "<i></i>", "<br>", width, height, points) +\
+          HTML_SUFFIX
       end
     end
 
     private
 
-    def process_row(row)
+    def self.process_row(row)
       row.map { |point| @points.include?(point) ? @pixel_on : @pixel_off }.join
     end
 
-    def canvas
-      0.upto(@height - 1).map { |y| 0.upto(@width - 1).map { |x| [x, y] } }
+    def self.canvas(width, height)
+      0.upto(height - 1).map { |y| 0.upto(width - 1).map { |x| [x, y] } }
     end
 
-    def process(on, off, separator)
+    def self.render(on, off, separator, width, height, points)
+      @points = points
       @pixel_on  = on
       @pixel_off = off
-      canvas.map { |row| process_row(row) }.join(separator)
+      self.canvas(width, height).map { |row| self.process_row(row) }.join(separator)
     end
   end
 
@@ -119,8 +112,8 @@ module Graphics
   end
 
   class Point
-    include Shape
     attr_reader :x, :y
+    include Shape
 
     def initialize(x, y)
       @x = x
@@ -143,15 +136,11 @@ module Graphics
         result
       end
     end
-
-    def to_s
-      @x.to_s + ',' + @y.to_s
-    end
   end
 
   class Line
-    include Shape
     attr_reader :from, :to
+    include Shape
 
     def initialize(point_a, point_b)
       if (point_a <=> point_b) > 0
@@ -169,11 +158,9 @@ module Graphics
 
     def pixels
       delta_x, delta_y = @to.x - @from.x, @to.y - @from.y
-
       if delta_x != 0
         slope = delta_y/delta_x.to_f
       end
-
       if slope
         rasterize([delta_x, delta_y].max, slope).map { |x, y| [from.x + x, from.y + y] }
       else
@@ -193,11 +180,10 @@ module Graphics
   end
 
   class Rectangle
-    include Shape
     attr_reader :left, :right, :top_left, :top_right, :bottom_right, :bottom_left
+    include Shape
 
     def initialize(point_a, point_b)
-      puts "pab #{@point_a} #{@point_b}"
       if (point_a <=> point_b) > 0
         @left  = point_b
         @right = point_a
@@ -205,7 +191,6 @@ module Graphics
         @left  = point_a
         @right = point_b
       end
-      puts "lr #{@left} #{@right}"
       determine_corners
     end
 
@@ -214,24 +199,17 @@ module Graphics
     end
 
     def pixels
-      puts "tl #{@top_left}"
-      z = [
+      [
         [@top_left    , @top_right],
         [@top_right   , @bottom_right],
         [@bottom_right, @bottom_left],
         [@bottom_left , @top_left]
-      ]
-      z.each { |zz| puts "#{zz[0]}, #{zz[1]}"}
-      z = z.map { |p| Line.new(*p) }
-      #.map(&:pixels).reduce &:+
-      z.each { |zz| puts "#{zz.from}, #{zz.to}"}
-      z.map(&:pixels).reduce &:+
+      ].map { |points| Line.new *points }.map(&:pixels).reduce &:+
     end
 
     private
 
-    def corners=(top_left, top_right, bottom_right, bottom_left)
-      puts "corners #{top_left} #{top_right} #{bottom_right} #{bottom_left}"
+    def corner_setter top_left, top_right, bottom_right, bottom_left
       @top_left     = top_left
       @top_right    = top_right
       @bottom_right = bottom_right
@@ -242,14 +220,10 @@ module Graphics
       other_left  = Point.new @left.x, @right.y
       other_right = Point.new @right.x, @left.y
       if @left.y < other_left.y
-        puts "dc #{@left}, #{other_right}, #{@right}, #{other_left}"
-        self.corners = @left, other_right, @right, other_left
+        corner_setter @left, other_right, @right, other_left
       else
-        puts "dc #{other_left}, #{@right}, #{other_right}, #{@left}"
-        corners = other_left, @right, other_right, @left
+        corner_setter other_left, @right, other_right, @left
       end
-
-      puts "top right #{@top_right}"
     end
   end
 end
